484aacf07f6e8d12f751769dff045cd6358f37cc
beheerder <info@berg-connect.nl>
2026-09-10 19:14:40 +0200

    chore: negative CI test (do not merge)

--- tests/Api.Tests/HealthEndpointTests.cs
+++ tests/Api.Tests/HealthEndpointTests.cs
@@ -19,6 +19,6 @@ public class HealthEndpointTests : IClassFixture<WebApplicationFactory<Program>>
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         var body = await response.Content.ReadAsStringAsync();
-        Assert.Contains("Healthy", body);
+        Assert.Contains("DeliberatelyBroken", body);
     }
 }
