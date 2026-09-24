49d7f9ad1262a291433110dc6b1c6b5ab46d4dbe
beheerder <info@berg-connect.nl>
2026-09-24 17:19:40 +0200

    test: shared-lib probe (komma terug) bewijst Worker-only

--- src/Shared/HealthCheck.cs
+++ src/Shared/HealthCheck.cs
@@ -2,5 +2,5 @@ namespace Shared;
 
 public static class HealthCheck
 {
-    public static string Build(string service) => $"Healthy {service}!";
+    public static string Build(string service) => $"Healthy, {service}!";
 }
