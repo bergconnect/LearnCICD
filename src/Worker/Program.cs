0bdeaa9ee0994c7282e815da256ab5e22a0d78bc
beheerder <info@berg-connect.nl>
2026-09-24 11:51:56 +0200

    test: beide projecten tegelijk voor matrix-bewijs

--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -2,7 +2,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/health", () => Results.Ok("Healthy, Roundtrip!"));
+app.MapGet("/health", () => Results.Ok("Healthy, Both!"));
 
 app.Run();
 
